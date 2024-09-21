683a93d17cf72ee1cf2b8a1e800147b5655cf338
joseph <younghyun7248@gmail.com>
2024-09-21 18:33:32 +0900

    add study files 09_21.env

--- 09_21/repeat.py
+++ 09_21/repeat.py
@@ -1,3 +1,12 @@
 """
 연수님 오늘은 복습입니다!!
+
+1. python 개발 환경 설치
+    1) python.org 에서 python 다운로드
+    2) Pycharm 다운로드
+    3) Git bash
+    4) 파이참 열어서 테스트 용 프로젝트 생성해보고 가상환경 폴더 만들기
+    5) 테스트 완료되면 git bash 로 PacharmProjects 폴더 열어서 git bash 로 clone
+    6) 이후의 수업에서는 git pull 만 반복하면 됨.
+
 """
