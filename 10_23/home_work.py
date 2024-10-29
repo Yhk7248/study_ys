05bfe0693296ed1151c94d18fc07480fde129b03
joseph <younghyun7248@gmail.com>
2024-10-29 19:57:33 +0900

    수업내용

--- 10_23/home_work.py
+++ 10_23/home_work.py
@@ -1,3 +1,24 @@
 """
 백준 9095번 다이나믹 프로그래밍 문제.
 """
+
+import sys
+
+if __name__ == "__main__":
+
+    test_case = int(input())
+
+    for _ in range(test_case):
+        N = int(input())
+        dp = [0] * (N-1)
+
+        for i in range(1, N+1):
+            if i == 1:
+                dp[i] = 1
+            elif i == 2:
+                dp[i] = 2
+            elif i == 3:
+                dp[i] = 4
+            else:
+                dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        print(dp[N])
